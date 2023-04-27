a0d7c881424fa15ebd7502faeb2fea756ec7470e
Miran <112928970+Mcfearless314@users.noreply.github.com>
2023-04-27 12:10:59 +0200

    implement initializable

--- src/GUI/Controller/TopBarView.java
+++ src/GUI/Controller/TopBarView.java
@@ -1,4 +1,13 @@
 package GUI.Controller;
 
-public class TopBarView {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TopBarView implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
