3c98b32d266f097a2b903caf970f473626114fbd
Miran <112928970+Mcfearless314@users.noreply.github.com>
2023-04-27 12:20:00 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("GUI/view/Index.fxml"));
+
+        primaryStage.setTitle("WUAV");
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(true);
+        primaryStage.setOnCloseRequest(event -> System.exit(0));
+        primaryStage.show();
+
     }
 }
