0c8564b0658844944ea5c84a9fe01a47443e8473
Miran <112928970+Mcfearless314@users.noreply.github.com>
2023-05-01 08:20:34 +0200

    Updated public to @FXML private

--- src/GUI/Controller/CaseHomePageView.java
+++ src/GUI/Controller/CaseHomePageView.java
@@ -1,11 +1,27 @@
 package GUI.Controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CaseHomePageView implements Initializable {
+    @FXML
+    private Label lblCaseName;
+    @FXML
+    private TextField txtReportName, txtSearchField, txtAddendumName;
+    @FXML
+    private TextArea txtReportDescription, txtAddendumDescription;
+    @FXML
+    private Button btnCreateNewReport, btnCreateNewAddendum;
+    @FXML
+    private TableView tblViewExistingReports;
+    @FXML
+    private TableColumn colReportName, colTechnician, colCreatedDate, colStatus;
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
--- src/GUI/Controller/SearchForCaseView.java
+++ src/GUI/Controller/SearchForCaseView.java
@@ -1,13 +1,31 @@
 package GUI.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class SearchForCaseView implements Initializable {
+    @FXML
+    private TextField txtCustomer, txtCustomerAddress, txtCaseName, txtTechnician;
+    @FXML
+    private DatePicker dpDate;
+    @FXML
+    private Button btnFilter;
+    @FXML
+    private TableView tblViewFilteredCases;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void handleFilter(ActionEvent actionEvent) {
     }
 }
