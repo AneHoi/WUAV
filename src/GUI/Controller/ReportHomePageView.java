4f72a24c97b5544e46b34e88c78d149edf11907c
Miran <112928970+Mcfearless314@users.noreply.github.com>
2023-05-01 08:20:56 +0200

    Updated public to @FXML private 2

--- src/GUI/Controller/ReportHomePageView.java
+++ src/GUI/Controller/ReportHomePageView.java
@@ -1,11 +1,19 @@
 package GUI.Controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ReportHomePageView implements Initializable {
+    @FXML
+    private Label lblCustomerName, lblReportName, lblCustomerAddress, lblCustomerEmail, lblCustomerTelephone, lblCaseName, lblCaseID, lblCaseCreated, lblCaseTechnicians, lblCaseContactPerson, lblReportDescription;
+    @FXML
+    private VBox vboxSectionAdding;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
