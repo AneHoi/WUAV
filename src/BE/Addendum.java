a43202253a96a41cdc076ea00c9c282f839b122f
Miran <112928970+Mcfearless314@users.noreply.github.com>
2023-05-03 22:27:24 +0200

    Update Addendum.java

--- src/BE/Addendum.java
+++ src/BE/Addendum.java
@@ -2,88 +2,21 @@ package BE;
 
 import java.time.LocalDate;
 
-public class Addendum {
-    private int addendumID;
-    private String addendumName;
-    private String addendumDescription;
-    private int reportID;
-    private int assignedTechnician;
-    private LocalDate createdDate;
-    private int logID;
-    private boolean isActive;
+public class Addendum extends Report {
 
-    public Addendum(int addendumID, String addendumName, String addendumDescription, int reportID, int assignedTechnician, LocalDate createdDate, int logID, boolean isActive) {
-        this.addendumID = addendumID;
-        this.addendumName = addendumName;
-        this.addendumDescription = addendumDescription;
-        this.reportID = reportID;
-        this.assignedTechnician = assignedTechnician;
-        this.createdDate = createdDate;
-        this.logID = logID;
-        this.isActive = isActive;
+    private int connectedReportID;
+
+    public Addendum(int addendumID, String addendumName, String addendumDescription, int reportID, String assignedTechnician, LocalDate createdDate, int logID, boolean isActive, int connectedReportID) {
+        super(addendumID, addendumName, addendumDescription, reportID, assignedTechnician, createdDate, logID, isActive);
+        this.connectedReportID = connectedReportID;
+
     }
 
-    public int getAddendumID() {
-        return addendumID;
+    public int getConnectedReportID() {
+        return connectedReportID;
     }
 
-    public void setAddendumID(int addendumID) {
-        this.addendumID = addendumID;
-    }
-
-    public String getAddendumName() {
-        return addendumName;
-    }
-
-    public void setAddendumName(String addendumName) {
-        this.addendumName = addendumName;
-    }
-
-    public String getAddendumDescription() {
-        return addendumDescription;
-    }
-
-    public void setAddendumDescription(String addendumDescription) {
-        this.addendumDescription = addendumDescription;
-    }
-
-    public int getReportID() {
-        return reportID;
-    }
-
-    public void setReportID(int reportID) {
-        this.reportID = reportID;
-    }
-
-    public int getAssignedTechnician() {
-        return assignedTechnician;
-    }
-
-    public void setAssignedTechnician(int assignedTechnician) {
-        this.assignedTechnician = assignedTechnician;
-    }
-
-    public LocalDate getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(LocalDate createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public int getLogID() {
-        return logID;
-    }
-
-    public void setLogID(int logID) {
-        this.logID = logID;
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
-    public void setActive(boolean active) {
-        isActive = active;
+    public void setConnectedReportID(int connectedReportID) {
+        this.connectedReportID = connectedReportID;
     }
 }
