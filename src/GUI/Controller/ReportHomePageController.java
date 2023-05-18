7989c6a6d4e43f81d1285ed870865352d7985c35
hjorthen1243 <114651351+hjorthen1243@users.noreply.github.com>
2023-05-18 09:00:06 +0200

    PDF vælg path

--- src/GUI/Controller/ReportHomePageController.java
+++ src/GUI/Controller/ReportHomePageController.java
@@ -24,12 +24,11 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -473,18 +472,32 @@ public class ReportHomePageController implements Initializable {
             closeReport();
         } else if (btnSubmitReportForReview.getText().equals("Generate PDF")) {
             //TODO make them chose destination and insert destination in generatePdf method
-            generatePDF();
+            generatePDF(getPath());
             //TODO open pdf
         }
-
     }
 
-    private void generatePDF() {
+    private void generatePDF(String path) {
         PDFGenerator pdfGenerator = new PDFGenerator();
 
-        pdfGenerator.generateReport(currentReport, currentCase, currentCustomer, textsAndImagesOnReportList());
+        pdfGenerator.generateReport(currentReport, currentCase, currentCustomer, textsAndImagesOnReportList(), path);
 
 
+    }
+    private String getPath(){
+        String path = "";
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Select a folder");
+        directoryChooser.getInitialDirectory();
+        if(btnSubmitReportForReview.getText().equals("Generate PDF")){
+            Stage stage = (Stage) btnSubmitReportForReview.getScene().getWindow();
+            File selectedFile = directoryChooser.showDialog(stage);
+            if(selectedFile != null){
+                path = selectedFile.getPath();
+            }
+        }
+        System.out.println(path);
+        return path;
     }
 
 
