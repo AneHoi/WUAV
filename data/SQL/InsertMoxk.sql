614a64d10bc99c00b954be358f37a178ef52eca4
TheDemon12345678910 <anehoimark@gmail.com>
2023-05-18 09:07:24 +0200

    updatedScripts

--- data/SQL/InsertMoxk.sql
+++ data/SQL/InsertMoxk.sql
@@ -1,3 +1,5 @@
+USE OW_WUAV_2;
+GO
 INSERT INTO User_Type(USER_TYPE_TYPE)
 VALUES('Admin')
 INSERT INTO User_Type VALUES('ProjectManager')
@@ -20,9 +22,11 @@ GO
 
 INSERT INTO Customer VALUES('Need Help co', 'Help Me street 1', 'needhelp@help.com', '91191191', '1234321', 'Corporate')
 
-INSERT INTO Case_ VALUES('Conference room set up', 'They need to be able to...', (SELECT User_ID FROM User_ WHERE [User_Name] = 'PM'), 1, (SELECT User_ID FROM User_ Where [User_Name] = 'T1'), convert(date, '2023-9-14'))
+INSERT INTO Case_ VALUES('Conference room set up', 'They need to be able to...', (SELECT User_ID FROM User_ WHERE [User_Name] = 'PM'), 1, (SELECT User_ID FROM User_ Where [User_Name] = 'T1'), convert(date, '2023-9-14'), null, null)
 GO
-INSERT INTO Case_ VALUES('Just an old case', 'They want to delete this case', (SELECT User_ID FROM User_ WHERE [User_Name] = 'PM'), 1, (SELECT User_ID FROM User_ Where [User_Name] = 'T1'), convert(date, '2003-9-14'))
+INSERT INTO Case_ VALUES('Just an old case', 'They want to delete this case', (SELECT User_ID FROM User_ WHERE [User_Name] = 'PM'), 1, (SELECT User_ID FROM User_ Where [User_Name] = 'T1'), convert(date, '2003-9-14'), CONVERT(date, '2004-9-14'), 1461)
+GO
+INSERT INTO Case_ VALUES('Just another old case', 'They want to delete this case', (SELECT User_ID FROM User_ WHERE [User_Name] = 'PM'), 1, (SELECT User_ID FROM User_ Where [User_Name] = 'T1'), convert(date, '2015-9-14'), CONVERT(date, '2016-9-14'), 1461)
 GO
 
 INSERT INTO Report VALUES('Conf room setup', 'Conf room need....', (SELECT DISTINCT User_ID FROM User_ WHERE [User_Name] = 'T1'), (SELECT DISTINCT Case_ID FROM Case_ WHERE Case_Name = 'Conference room set up'), convert(date, '2023-9-18'), 1, 'Open')
@@ -47,4 +51,6 @@ GO
 SELECT * FROM Report;
 GO
 
+GO
+USE master;
 GO
