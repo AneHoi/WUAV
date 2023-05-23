c2994aa7296f9b3a6a2aeca8e24aafdb0fad8f50
TheDemon12345678910 <anehoimark@gmail.com>
2023-05-23 12:09:55 +0200

    refactoring

--- src/GUI/Controller/AddIconController.java
+++ src/GUI/Controller/AddIconController.java
@@ -49,6 +49,7 @@ public class AddIconController implements Initializable {
         vBox.getChildren().remove(lblImage);
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Select Image");
+        //TODO remove this line when Handing in. It is for testing the program.
         fileChooser.setInitialDirectory(new File("..\\WUAV\\data\\Images\\DrawingProgramTest"));
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"));
         Stage stage = (Stage) btnChooseImage.getScene().getWindow();
