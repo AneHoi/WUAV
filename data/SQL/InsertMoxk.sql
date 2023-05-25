89eb8a13d3381fbd460af63a3ed73714ec21a695
Miran <112928970+Mcfearless314@users.noreply.github.com>
2023-05-25 09:57:42 +0200

    Updated CreateDB and Mock

--- data/SQL/InsertMoxk.sql
+++ data/SQL/InsertMoxk.sql
@@ -1,13 +1,8 @@
 USE OW_WUAV_x;
 GO
-INSERT INTO User_Type(USER_TYPE_TYPE)
-VALUES('Admin')
-INSERT INTO User_Type VALUES('ProjectManager')
-INSERT INTO User_Type VALUES('Technician')
-INSERT INTO User_Type VALUES('SalesRepresentative')
 
-INSERT INTO User_ VALUES('Test','Test', (SELECT DISTINCT User_Type_ID FROM User_Type WHERE USER_TYPE_TYPE = 'Admin'), '34567', '23456', 1, null)
-INSERT INTO User_ VALUES('Tech1','Tech1', (SELECT DISTINCT User_Type_ID FROM User_Type WHERE USER_TYPE_TYPE = 'Technician'), '12@12.12', '12121212', 3, null)
+INSERT INTO User_ VALUES('Test','Test', 1, '34567', '23456', 1, null)
+INSERT INTO User_ VALUES('Tech1','Tech1', 3, '12@12.12', '12121212', 3, null)
 GO
 
 INSERT INTO User_Passwords VALUES((SELECT DISTINCT User_ID FROM User_  WHERE User_Name = 'Test'), 'Test', '$2a$16$dQY0YELZUXi6qk/HL9gCd.mqkYyidX3AnFsOERgwjlc5n5ExXbUuK', '$2a$16$dQY0YELZUXi6qk/HL9gCd.')
@@ -23,18 +18,15 @@ GO
 INSERT INTO Case_ VALUES('Just another old case', 'They want to delete this case', (SELECT User_ID FROM User_ WHERE [User_Name] = 'Test'), 1, (SELECT User_ID FROM User_ Where [User_Name] = 'Tech1'), convert(date, '2015-9-14'), CONVERT(date, '2016-9-14'), 1461)
 GO
 
-INSERT INTO Report VALUES('Conf room setup', 'Conf room need....', (SELECT DISTINCT User_ID FROM User_ WHERE [User_Name] = 'Tech1'), (SELECT DISTINCT Case_ID FROM Case_ WHERE Case_Name = 'Conference room set up'), convert(date, '2023-9-18'), 1, 'Open')
+INSERT INTO Report VALUES('Conf room setup', 'Conf room need....', (SELECT DISTINCT User_ID FROM User_ WHERE [User_Name] = 'Tech1'), (SELECT DISTINCT Case_ID FROM Case_ WHERE Case_Name = 'Conference room set up'), convert(date, '2023-9-18'), 'Open')
 GO
 
-INSERT INTO Report VALUES('Meeting Room Setup', 'Meeting Room needs....', (SELECT DISTINCT User_ID FROM User_ WHERE [User_Name] = 'Tech1'), (SELECT DISTINCT Case_ID FROM Case_ WHERE Case_Name = 'Conference room set up'), convert(date, '2023-9-18'), 1, 'Submitted For Review')
+INSERT INTO Report VALUES('Meeting Room Setup', 'Meeting Room needs....', (SELECT DISTINCT User_ID FROM User_ WHERE [User_Name] = 'Tech1'), (SELECT DISTINCT Case_ID FROM Case_ WHERE Case_Name = 'Conference room set up'), convert(date, '2023-9-18'), 'Submitted For Review')
 GO
-INSERT INTO Report VALUES('Meeting Room Setup', 'Meeting Room needs....', (SELECT DISTINCT User_ID FROM User_ WHERE [User_Name] = 'Tech1'), (SELECT DISTINCT Case_ID FROM Case_ WHERE Case_Name = 'Conference room set up'), convert(date, '2023-9-18'), 1, 'Closed')
+INSERT INTO Report VALUES('Meeting Room Setup', 'Meeting Room needs....', (SELECT DISTINCT User_ID FROM User_ WHERE [User_Name] = 'Tech1'), (SELECT DISTINCT Case_ID FROM Case_ WHERE Case_Name = 'Conference room set up'), convert(date, '2023-9-18'), 'Closed')
 GO
 
 INSERT INTO Technicians_Assigned_To_Case VALUES(2,1)
-
-SELECT * FROM User_Type;
-GO
 
 SELECT * FROM User_;
 GO
